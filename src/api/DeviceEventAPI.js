ce7f0717a84bc1df5fcd80a505040604c311ac18
jamaludin sanur <jamaludinsanur3@gmail.com>
2022-09-20 06:12:29 +0800

    Change server url to use remote server instead of localhost

--- src/api/DeviceEventAPI.js
+++ src/api/DeviceEventAPI.js
@@ -1,22 +1,23 @@
 const axios = require('axios').default;
+const URL_SERVER = process.env.REACT_APP_URL_SERVER;
 
 export async function getAllDeviceEvent(idDeviceEvent) {
-    const response = await axios.get(`http://localhost:9000/rest/public/deviceEvent/all`);
+    const response = await axios.get(URL_SERVER+`/rest/public/deviceEvent/all`);
     return response.data;
 }
 
 export async function getSingleDeviceEvent(idDeviceEvent) {
-    const response = await axios.get(`http://localhost:9000/rest/public/deviceEvent/single?idDeviceEvent=${idDeviceEvent}`);
+    const response = await axios.get(URL_SERVER+`/rest/public/deviceEvent/single?idDeviceEvent=${idDeviceEvent}`);
     return response.data;
 }
 
 export async function getSingleDeviceEventDetail(idDeviceEvent) {
-    const response = await axios.get(`http://localhost:9000/rest/public/deviceEvent/single/detail?idDeviceEvent=${idDeviceEvent}`);
+    const response = await axios.get(URL_SERVER+`/rest/public/deviceEvent/single/detail?idDeviceEvent=${idDeviceEvent}`);
     return response.data;
 }
 
 export async function upsertDeviceEventAction(data) {
-    const response = axios.post('http://localhost:9000/rest/public/deviceEvent/upsert', data);
+    const response = axios.post(URL_SERVER+'/rest/public/deviceEvent/upsert', data);
     return response.data;
 }
 
